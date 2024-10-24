abbb5edc1cd328732cde7280be09243fb0c25bc0
cmlr4136 <cmlr4136@leeds.ac.uk>
2024-10-24 21:47:44 +0000

    check 1

--- code.py
+++ code.py
@@ -1,1 +1,19 @@
+seqLen = int(input("Please enter how many integers of the Recaman Sequence you would like to see: "))
+currentNum = 0
+nums = []
+for k in range(seqLen):
+    if currentNum - k > 0:
+        if currentNum - k not in nums:
+         currentNum = currentNum - k
+         nums.append(currentNum)
+        else:
+            currentNum = currentNum + k
+    else:
+        currentNum = currentNum + k
+        if currentNum not in nums:
+            nums.append(currentNum)
+    print(currentNum)
 
+    
+
+    
